bc6f0f80cc2bba1669010640cc71667c3fb03d4b
tales1982 <142450583+TalesPortfolio@users.noreply.github.com>
2025-11-02 23:25:04 +0100

    Adding text editor version 2 is not complete.

---
+++ BALTA-IO/TextEditorV2/Menu.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TextEditorV2
+{
+    public class Menu
+    {
+        public void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("ola teste");
+
+        }
+    }
+}
---
+++ BALTA-IO/TextEditorV2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TextEditorV2;
+
+var menu = new Menu();
+
+
+menu.Show();
